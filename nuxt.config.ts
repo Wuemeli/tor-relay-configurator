a590d25e495e0c641055780c5b9a0c322e7c256c
Wuemeli <54443643+Wuemeli@users.noreply.github.com>
2026-01-01 14:02:47 +0100

    twacking :3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   ssr: true,
   devtools: true,
+
   app: {
     head: {
       charset: "utf-8",
@@ -10,16 +11,10 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "en",
       },
-      script: [
-        {
-          defer: true,
-          dataDomain: "tor-relay.dev",
-          src: "https://googleisbad.wuemeli.com/js/script.outbound-links.tagged-events.js",
-        },
-      ],
     },
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
+
   vite: { plugins: [tailwindcss()] },
   css: ["~/assets/css/main.css"],
 
@@ -30,5 +25,13 @@ export default defineNuxtConfig({
       crawlLinks: true,
       routes: ["/"],
     },
+  },
+
+  modules: ["nuxt-umami"],
+
+  umami: {
+    id: "90c7973a-08b5-4e41-bc7e-d29800e1fdc0",
+    host: "https://hahaitwackedyou.wuemeli.com",
+    autoTrack: true,
   },
 });
